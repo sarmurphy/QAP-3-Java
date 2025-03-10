aab6dcd490015795ca9f52e1da8391c1c0c83b73
Sarah Murphy <sarahelizabeth.murphy@keyin.com>
2025-03-10 14:55:51 -0230

    Added Person class

---
+++ Problem 1/Person.java
@@ -0,0 +1,42 @@
+public class Person {
+    protected String myName;
+    protected int myAge;
+    protected String myGender;
+
+    // Constructor
+    public Person(String name, int age, String gender) {
+        myName = name;
+        myAge = age;
+        myGender = gender;
+    }
+
+    // Getters and Setters
+    public String getName() {
+        return myName;
+    }
+
+    public int getAge() {
+        return myAge;
+    }
+
+    public String getGender() {
+        return myGender;
+    }
+
+    public void setName(String name) {
+        myName = name;
+    }
+
+    public void setAge(int age) {
+        myAge = age;
+    }
+
+    public void setGender(String gender) {
+        myGender = gender;
+    }
+
+    // toString method to return values in a string format
+    public String toString() {
+        return myName + ", age: " + myAge + ", gender: " + myGender;
+    }
+}

88c9c5a7b1e53e1fc151eb5d4c5570b662d5a163
Sarah Murphy <sarahelizabeth.murphy@keyin.com>
2025-03-10 16:07:07 -0230

    Added space between formatted outputs

--- Problem 1/Demo.java
+++ Problem 1/Demo.java
@@ -1,15 +1,20 @@
 public class Demo {
     public static void main(String[] args) {
+        System.out.println();
         Person bob = new Person("Coach Bob", 27, "M");
         System.out.println(bob);
 
+        System.out.println();
         Student lynne = new Student("Lynne Brooke", 16, "F", "HS95129", 3.5);
         System.out.println(lynne);
 
+        System.out.println();
         Teacher mrJava = new Teacher("Duke Java", 34, "M", "Computer Science", 50000);
         System.out.println(mrJava);
 
+        System.out.println();
         CollegeStudent ima = new CollegeStudent("Ima Frosh", 18, "F", "UCB123", 4.0, 1, "English");
         System.out.println(ima);
+        System.out.println();
     }
 }
